19ef651914734bd14a0664009dfe839ddb273ecd
Bryan Hunter <bdhunter3141@gmail.com>
2021-07-25 21:15:06 -0400

    feat: 🎸 completed maintenance site

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Head from 'next/head'
+import { up } from 'styled-breakpoints'
 import styled from 'styled-components'
 import backgroundImage from '../public/assets/background.jpg'
+import { Container } from '../components'
 
 // styles
 const BackgroundLayout = styled.div`
@@ -25,35 +27,70 @@ const ContentLayout = styled.main`
 `
 const TextLayout = styled.div`
   background-color: #ffffff;
-  padding: 3rem 6rem;
   border-radius: 10px;
+  max-width: 768px;
+  margin: 1rem;
+  padding: 2rem;
+
+  ${up('md')} {
+    margin: 3rem 6rem;
+    padding: 3rem 6rem;
+  }
+`
+const SignatureLayout = styled.p`
+  font-family: 'La Belle Aurore', cursive;
+  font-size: 1.2rem;
 `
 
 export default function Home() {
   return (
     <>
-      {/* TODO: Separate out Head */}
       <Head>
         <title>AltJolly - Home</title>
-        {/* TODO: Favicon */}
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap" rel="stylesheet" />
       </Head>
 
       <BackgroundLayout>
         <Image src={backgroundImage} layout="fill" objectFit="cover" />
       </BackgroundLayout>
 
-      <ContentLayout>
-        <TextLayout>
-          <Image
-            src="/assets/logo.png"
-            alt="AltJolly Logo"
-            width={200}
-            height={44}
-          />
-          <h1>Hello World</h1>
-        </TextLayout>
-      </ContentLayout>
+      <Container>
+        <ContentLayout>
+          <TextLayout>
+            <Image
+              src="/assets/logo.png"
+              alt="AltJolly Logo"
+              width={200}
+              height={44}
+            />
+
+            <h1>We are currently undergoing maintenance</h1>
+
+            <p>Thank you so much for your patience! We will be back online as soon as we can.</p>
+            {/* <h1>Thank you!</h1>
+            <p>
+              We sincerely thank you for being a customer of AltJolly and we appreciate your support.
+              Our amazing customers are the reason that we do what we do.
+            </p>
+            <p>
+              Unfortunately, we have recently had some trouble remaining profitable and have been forced to close our doors.
+              We are so sad to have to say goodbye but we have had such an incredible experience serving you and all of our
+              incredible customers and wouldn't trade that time for the world.
+            </p>
+            <p>
+              Wishing you nothing but the best!
+            </p>
+            <p>
+              Lots of love,
+            </p>
+            */}
+            <SignatureLayout>The AltJolly Family</SignatureLayout>
+          </TextLayout>
+        </ContentLayout>
+      </Container>
     </>
   )
 }
